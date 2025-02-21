75e6b9510c492e3417fe564b56551ead87894208
UnderscoreKilburn <trtv@live.fr>
2025-02-22 00:38:04 +0100

    👋 Fix broken right click interaction with Create's configurable blocks

--- src/main/java/com/petrolpark/destroy/mixin/ValueSettingsPacketMixin.java
+++ src/main/java/com/petrolpark/destroy/mixin/ValueSettingsPacketMixin.java
@@ -33,6 +33,7 @@ public class ValueSettingsPacketMixin {
 				continue;
 			if (hand != null) {
 				valueSettingsBehaviour.onShortInteract(player, hand, side);
+				return;
 			};
             if (valueSettingsBehaviour instanceof SmartValueSettingsBehaviour smartValueSettingsBehaviour) {
                 smartValueSettingsBehaviour.acceptAccessInformation(hand, side);
