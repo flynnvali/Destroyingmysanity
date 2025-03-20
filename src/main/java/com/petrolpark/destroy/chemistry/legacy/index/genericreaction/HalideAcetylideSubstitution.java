95ffd139fb923d31fce5cf362de5ff424f31e122
Flynnvali <flynnherr@gmail.com>
2025-03-20 00:22:33 -0500

    - beginning of acetylides

--- src/main/java/com/petrolpark/destroy/chemistry/legacy/index/genericreaction/HalideAcetylideSubstitution.java
+++ src/main/java/com/petrolpark/destroy/chemistry/legacy/index/genericreaction/HalideAcetylideSubstitution.java
@@ -1,8 +1,11 @@
 package com.petrolpark.destroy.chemistry.legacy.index.genericreaction;
 
 import com.petrolpark.destroy.Destroy;
-import com.petrolpark.destroy.chemistry.legacy.*;
 import com.petrolpark.destroy.chemistry.legacy.LegacyBond.BondType;
+import com.petrolpark.destroy.chemistry.legacy.LegacyReaction;
+import com.petrolpark.destroy.chemistry.legacy.LegacyMolecularStructure;
+import com.petrolpark.destroy.chemistry.legacy.ReadOnlyMixture;
+import com.petrolpark.destroy.chemistry.legacy.LegacyElement;
 import com.petrolpark.destroy.chemistry.legacy.genericreaction.DoubleGroupGenericReaction;
 import com.petrolpark.destroy.chemistry.legacy.genericreaction.GenericReactant;
 import com.petrolpark.destroy.chemistry.legacy.index.DestroyMolecules;
@@ -23,44 +26,69 @@ public class HalideAcetylideSubstitution extends DoubleGroupGenericReaction<Hali
     }
 
 
+   // @Override
+   // public LegacyReaction generateReaction(GenericReactant<HalideGroup> firstReactant, GenericReactant<AcetylideGroup> secondReactant) {
+    //    LegacyMolecularStructure halideStructureCopy = firstReactant.getMolecule().shallowCopyStructure();
+    //    HalideGroup halideGroup = firstReactant.getGroup();
+    //    LegacyMolecularStructure acetylideStructureCopy = secondReactant.getMolecule().shallowCopyStructure();
+      //  AcetylideGroup acetylideGroup = secondReactant.getGroup();
+
+      //  acetylideStructureCopy.moveTo(acetylideGroup.neutralCarbon);
+       // acetylideStructureCopy.remove(acetylideGroup.carbonWithCharge);
+      //  acetylideStructureCopy.addAtom(new LegacyAtom(LegacyElement.CARBON), BondType.TRIPLE);
+
+      //  halideStructureCopy.moveTo(HalideGroup.getCarbon);
+      //  halideStructureCopy.remove(HalideGroup.getHalogen);
+
+      //  LegacySpecies ccBond = moleculeBuilder().structure(LegacyMolecularStructure.joinFormulae(halideStructureCopy, acetylideStructureCopy, BondType.SINGLE)).build();
+
+
+
+      //  ReactionBuilder builder = reactionBuilder()
+
+         //       .addReactant(firstReactant.getMolecule())
+         //       .addReactant(secondReactant.getMolecule())
+        //        .addProduct(ccBond)
+          //      .addProduct(getIon(halideGroup.halogen));
+           //     return builder.build();
     @Override
     public LegacyReaction generateReaction(GenericReactant<HalideGroup> firstReactant, GenericReactant<AcetylideGroup> secondReactant) {
-        LegacyMolecularStructure halideStructureCopy = firstReactant.getMolecule().shallowCopyStructure();
-        HalideGroup halideGroup = firstReactant.getGroup();
-        LegacyMolecularStructure acetylideStructureCopy = secondReactant.getMolecule().shallowCopyStructure();
-        AcetylideGroup acetylideGroup = secondReactant.getGroup();
-
-        acetylideStructureCopy.moveTo(acetylideGroup.carbonWithCharge);
-        acetylideStructureCopy.moveTo(acetylideGroup.carbonWithCharge);
-
-        halideStructureCopy.moveTo(HalideGroup.getHalogen);
-        halideStructureCopy.remove(HalideGroup.getCarbon);
-
-        LegacySpecies ccBond = moleculeBuilder().structure(LegacyMolecularStructure.joinFormulae(halideStructureCopy, acetylideStructureCopy, BondType.SINGLE)).build();
-
-
-
+        LegacyMolecularStructure halideGroup = firstReactant.molecule.shallowCopyStructure();
+        // HalideGroup halide = firstReactant.getGroup();
+        LegacyMolecularStructure acetylideGroup = secondReactant.molecule.shallowCopyStructure();
+        halideGroup.moveTo(firstReactant.group.carbon)
+                .remove(firstReactant.group.halogen);
+       acetylideGroup.moveTo(secondReactant.group.neutralCarbon)
+                .remove(secondReactant.group.carbonWithCharge)
+                .addAtom(LegacyElement.CARBON, BondType.TRIPLE);
+       // acetylideGroup.moveTo(secondReactant.group.carbonWithCharge);
+        LegacyMolecularStructure product = LegacyMolecularStructure.joinFormulae(halideGroup, acetylideGroup, BondType.SINGLE);
         return reactionBuilder()
-                .addReactant(firstReactant.getMolecule())
-                .addReactant(secondReactant.getMolecule())
-                .addProduct(ccBond)
-                .addProduct(getIon(halideGroup.halogen))
-                //TODO kinetics
+                .addReactant(firstReactant.molecule)
+                .addReactant(secondReactant.molecule)
+                .addProduct(moleculeBuilder().structure(product).build())
+                .addProduct(DestroyMolecules.CHLORIDE)
                 .build();
     };
 
-    public LegacySpecies getIon(LegacyAtom atom) {
-        switch (atom.getElement()) {
-            case FLUORINE:
-                return DestroyMolecules.FLUORIDE;
-            case CHLORINE:
-                return DestroyMolecules.CHLORIDE;
-            case IODINE:
-                return DestroyMolecules.IODIDE;
-            default:
-                throw new GenericReactionGenerationException(atom.getElement().toString()+" is not a halogen.");
-        }
-    };
+
+   // };
+
+  //  public void transform(LegacyReaction.ReactionBuilder builder, HalideGroup group) {};
+
+   // public LegacySpecies getIon(LegacyAtom atom) {
+   //     switch (atom.getElement()) {
+   //         case FLUORINE:
+   //             return DestroyMolecules.FLUORIDE;
+    //        case CHLORINE:
+  //              return DestroyMolecules.CHLORIDE;
+   //         case IODINE:
+    //            return DestroyMolecules.IODIDE;
+   //         default:
+      //          throw new GenericReactionGenerationException(atom.getElement().toString()+" is not a halogen.");
+
+
+
 
 
 
