549c993911ef068418069fc48c7c7f95d29fb3cb
Flynnvali <flynnherr@gmail.com>
2025-03-22 02:11:09 -0500

    - added and promptly commented out double elimation to serve as a warning for any future idiots such as myself who attempt to add it themselves

--- src/main/java/com/petrolpark/destroy/chemistry/legacy/index/DestroyGenericReactions.java
+++ src/main/java/com/petrolpark/destroy/chemistry/legacy/index/DestroyGenericReactions.java
@@ -11,6 +11,7 @@ public class DestroyGenericReactions {
     public static final AcylChlorideEsterification ACYL_CHLORIDE_ESTERIFICATION = new AcylChlorideEsterification();
     public static final AcylChlorideFormation ACYL_CHLORIDE_FORMATION = new AcylChlorideFormation();
     public static final AcylChlorideHydrolysis ACYL_CHLORIDE_HYDROLYSIS = new AcylChlorideHydrolysis();
+    // public static final DoubleElimination DOUBLE_ELIMINATION = new DoubleElimination(); // this sits here as a monument to my hubris. turns out, its absolutely horrible for performance in combination with my acetylide additions and also made a bit redundant by those said changes as well! do not uncomment this unless the api is ***much*** more stable than it is as of 3/22/25. otherwise bear my hubris
     public static final AlcoholDehydration ALCOHOL_DEHYDRATION = new AlcoholDehydration();
     public static final AlcoholOxidation ALCOHOL_OXIDATION = new AlcoholOxidation();
     public static final AldehydeOxidation ALDEHYDE_OXIDATION = new AldehydeOxidation();
