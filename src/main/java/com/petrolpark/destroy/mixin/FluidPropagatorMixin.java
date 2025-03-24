224c4110621552e0692b3351dba6532aceba0e85
Flynnvali <flynnherr@gmail.com>
2025-03-24 18:31:02 -0500

    more kilbu

--- src/main/java/com/petrolpark/destroy/mixin/FluidPropagatorMixin.java
+++ src/main/java/com/petrolpark/destroy/mixin/FluidPropagatorMixin.java
@@ -1,5 +1,10 @@
 package com.petrolpark.destroy.mixin;
 
+<<<<<<< HEAD
+=======
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+>>>>>>> parent of f19b87f6 (🛢️ Temporary fix for TFMG compatibility)
 import com.petrolpark.destroy.DestroyBlocks;
 import com.simibubi.create.content.fluids.FluidPropagator;
 import com.tterrag.registrate.util.entry.BlockEntry;
@@ -20,8 +25,14 @@ public class FluidPropagatorMixin {
         ),
         remap=false
     )
+<<<<<<< HEAD
     @SuppressWarnings("rawtypes")
     private static boolean matchOtherPumps(BlockEntry instance, BlockState state) {
         return instance.has(state) || DestroyBlocks.CREATIVE_PUMP.has(state);
     };
+=======
+    private static boolean matchOtherPumps(BlockEntry<?> instance, BlockState state, Operation<Boolean> original) {
+        return DestroyBlocks.CREATIVE_PUMP.has(state) || original.call(instance, state);
+    }
+>>>>>>> parent of f19b87f6 (🛢️ Temporary fix for TFMG compatibility)
 }
