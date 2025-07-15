3dc550d44eb71987b1c57ac1b35e8ea43d9256ba
Flynnvali <flynnherr@gmail.com>
2025-07-14 20:54:42 -0500

    - fixed import shit

--- src/main/java/com/petrolpark/destroy/chemistry/legacy/index/DestroyGenericReactions.java
+++ src/main/java/com/petrolpark/destroy/chemistry/legacy/index/DestroyGenericReactions.java
@@ -6,12 +6,15 @@ import com.petrolpark.destroy.chemistry.legacy.index.genericreaction.*;
 
 public class DestroyGenericReactions {
 
+    public static final AcetylideHydrolysis ACETYLIDE_HYDRATION = new AcetylideHydrolysis();
+    public static final AcetylideNucleophilicAddition ACETYLIDE_ADDITION = new AcetylideNucleophilicAddition();
     public static final AcylChlorideEsterification ACYL_CHLORIDE_ESTERIFICATION = new AcylChlorideEsterification();
     public static final AcylChlorideFormation ACYL_CHLORIDE_FORMATION = new AcylChlorideFormation();
     public static final AcylChlorideHydrolysis ACYL_CHLORIDE_HYDROLYSIS = new AcylChlorideHydrolysis();
     public static final AlcoholDehydration ALCOHOL_DEHYDRATION = new AlcoholDehydration();
     public static final AlcoholOxidation ALCOHOL_OXIDATION = new AlcoholOxidation();
     public static final AldehydeOxidation ALDEHYDE_OXIDATION = new AldehydeOxidation();
+    public static final HydrideAlkyneDeprotonation HYDRIDE_ALKYNE_DEPROTONATION = new HydrideAlkyneDeprotonation();
     public static final ElectrophilicChlorination ALKENE_CHLORINATION = new ElectrophilicChlorination(false);
     public static final ElectrophilicChlorohydrination ALKENE_CHLOROHYDRINATION = new ElectrophilicChlorohydrination(false);
     public static final SaturatedCarbonHydrolysis ALKENE_HYDROLYSIS = new SaturatedCarbonHydrolysis(false);
@@ -24,8 +27,9 @@ public class DestroyGenericReactions {
     public static final ElectrophilicIodination ALKENE_IODINATION = new ElectrophilicIodination(false);
     public static final AlkoxideProtonation ALKOXIDE_PROTONATION = new AlkoxideProtonation();
     public static final ElectrophilicChlorination ALKYNE_CHLORINATION = new ElectrophilicChlorination(true);
-    public static final ElectrophilicChlorohydrination ALKYNE_CHLOROHYDRINATION = new ElectrophilicChlorohydrination(true);
-    public static final SaturatedCarbonHydrolysis ALKYNE_HYDROLYSIS = new SaturatedCarbonHydrolysis(true);
+    public static final AlkyneChlorohydrination ALKYNE_CHLOROHYDRINATION = new AlkyneChlorohydrination();
+    public static final AlkyneHydrolysis ALKYNE_HYDROLYSIS = new AlkyneHydrolysis();
+    public static final AlkynePartialHydrogenation ALKYNE_DISSOLVING_METAL_REDUCTION = new AlkynePartialHydrogenation();
     //public static final ElectrophilicHydroboration ALKYNE_HYDROBORATION = new ElectrophilicHydroboration(true); // see alkene hydroboration
     public static final AlkyneHydroborationOxidation ALKYNE_HYDROBORATION_OXIDATION = new AlkyneHydroborationOxidation();
     //public static final BoraneElectrophilicHydroboration ALKYNE_HYDROBORATION_WITH_BORANE = new BoraneElectrophilicHydroboration(true); // unneeded thanks to simplified Hydroboration Oxidation reaction
@@ -43,6 +47,7 @@ public class DestroyGenericReactions {
     public static final CyanamideAddition CYANAMIDE_ADDITION = new CyanamideAddition();
     public static final CarboxylicAcidEsterification CARBOXYLIC_ACID_ESTERIFICATION = new CarboxylicAcidEsterification();
     public static final CyanideNucleophilicAddition CYANIDE_NUCLEOPHILIC_ADDITION = new CyanideNucleophilicAddition();
+    public static final HalideAcetylideSubstitution HALIDE_ACETYLIDE_SUBSTITUTION = new HalideAcetylideSubstitution();
     public static final HalideAmineSubstitution HALIDE_AMINE_SUBSTITUION = new HalideAmineSubstitution();
     public static final HalideAmmoniaSubstitution HALIDE_AMMONIA_SUBSTITUTION = new HalideAmmoniaSubstitution();
     public static final HalideCyanideSubstitution HALIDE_CYANIDE_SUBSTITUTION = new HalideCyanideSubstitution();
@@ -53,6 +58,8 @@ public class DestroyGenericReactions {
     public static final NitroHydrogenation NITRO_HYDROGENATION = new NitroHydrogenation();
     public static final ThionylChlorideSubstitution THIONYL_CHLORIDE_SUBSTITUTION = new ThionylChlorideSubstitution();
     public static final WolffKishnerReduction WOLFF_KISHNER_REDUCTION = new WolffKishnerReduction();
+    public static final GeminalDihalideHydroxideSubstitution GEMINAL_DIHALIDE_HYDROXIDE_SUBSTITUTION = new GeminalDihalideHydroxideSubstitution();
+    public static final LeukartReaction LEUKART_REACTION = new LeukartReaction();
 
     public static void register() {};
 };
